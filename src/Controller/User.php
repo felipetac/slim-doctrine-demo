30ada61fb779889d6d7ba448516507a20f9dc243
Felipe Toscano <felipe.toscano@gmail.com>
2018-10-15 13:14:36 -0300

    Correção de espaço no declare strict

--- src/Controller/User.php
+++ src/Controller/User.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
 namespace App\Controller;
 
 use Psr\Container\ContainerInterface;
--- tests/Functional/EndToEndTest.php
+++ tests/Functional/EndToEndTest.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
 namespace App\Tests\Functional;
 
 use Doctrine\ORM\EntityManager;
